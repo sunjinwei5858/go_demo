bdd02e2999bf95d3058ce82fcd31b5646a3404ef
sunjinwei <sunjinwei5828@163.com>
2022-09-12 13:56:09 +0800

    基本类型转string：fmt.Sprintln

--- demo01/hello.go
+++ demo01/hello.go
@@ -5,10 +5,27 @@ import (
 	"math"
 )
 
+// 基本类型转string：使用fmt.Sprintln()
+func int2String(a int) string {
+	return fmt.Sprintln(a)
+}
+
+func bol2String(a bool) string {
+	return fmt.Sprintln(a)
+}
+
 func main() {
+
 	fmt.Println("hello world")
 
 	// https://tour.go-zh.org/basics/2
 	result := math.Abs(-19)
 	fmt.Println(result)
+
+	s := int2String(2)
+	fmt.Println("int格式化的字符串=", s)
+
+	b := bol2String(true)
+	fmt.Println("布尔格式化的字符串=", b)
+
 }
