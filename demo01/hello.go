808019b2d6c49bb317100feff1745d653e5607f6
sunjinwei <sunjinwei5828@163.com>
2022-09-12 09:27:21 +0800

    go官方编程指南：https://tour.go-zh.org/basics/2

--- demo01/hello.go
+++ demo01/hello.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func main() {
-	fmt.Print("hello world")
+	fmt.Println("hello world")
+
+	// https://tour.go-zh.org/basics/2
+	result := math.Abs(-19)
+	fmt.Println(result)
 }
